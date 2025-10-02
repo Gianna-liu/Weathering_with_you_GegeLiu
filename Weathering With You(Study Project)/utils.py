e1d0ffc5bedc0f0acf205c621dd4c8896a8bb767
Gege.Liu <giannaliu23@gmail.com>
2025-10-02 16:06:15 +0200

    fix minor bugs

--- Weathering With You(Study Project)/utils.py
+++ Weathering With You(Study Project)/utils.py
@@ -4,6 +4,6 @@ import streamlit as st
 @st.cache_data
 def load_data():
     """Load data from a CSV file."""
-    df = pd.read_csv('data/open-meteo-subset.csv')
+    df = pd.read_csv('../data/open-meteo-subset.csv')
     df['time'] = pd.to_datetime(df['time'])
     return df
