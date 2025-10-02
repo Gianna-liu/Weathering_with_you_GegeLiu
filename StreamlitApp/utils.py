e4f001dc0cbad5594dd3e6055ac2900ae6133777
Gege.Liu <giannaliu23@gmail.com>
2025-10-02 22:46:52 +0200

    change the csv path

--- StreamlitApp/utils.py
+++ StreamlitApp/utils.py
@@ -6,8 +6,13 @@ from pathlib import Path
 def load_data(filename: str) -> pd.DataFrame:
     """Load data from a CSV file."""
     script_path = Path(__file__).resolve()
-    repo_root = script_path.parent.parent  
-    file_path = repo_root / "data" / filename
+    current = script_path.parent
+    while current != current.parent:
+        if (current / ".git").exists():
+            return current
+        current = current.parent
+    file_path = current / "StreamlitApp" / "data" / filename
+
     # df = pd.read_csv('data/open-meteo-subset.csv')
     # df['time'] = pd.to_datetime(df['time'])
     return pd.read_csv(file_path)
